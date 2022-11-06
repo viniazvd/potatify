066ec3dc73d241b61e2de207816a120bfe9f2c66
Allan Lima <allan.lima@seedz.ag>
2022-11-06 02:05:47 -0300

    Adiciona uma nova sugest~ao de card

---
+++ src/components/SCard/SCardV2.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card-shadows card-colors" :class="classes">
+    <slot />
+  </div>
+</template>
+<script lang="ts" setup>
+
+import {computed} from "vue";
+
+const props = defineProps({
+  noPadding: { type: Boolean, default: false },
+  backgroundColor: { type: String, default: "" }
+})
+
+const classes = computed(() => ({
+  "card-padding": !props.noPadding,
+  [props.backgroundColor]: props.backgroundColor
+}))
+
+</script>
+<style lang="postcss" scoped>
+
+.card-shadows {
+  @apply shadow sm:rounded-md dark:shadow-lg
+}
+
+.card-colors {
+  @apply ring-1 ring-black/5 bg-white dark:bg-stone-800
+}
+
+.card-padding {
+  @apply px-4 py-5 sm:px-6;
+}
+
+</style>
