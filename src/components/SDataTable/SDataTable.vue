6b809d17f36dbfb84a3621dc02a5cad4c17d7458
Allan Lima <allan.lima@seedz.ag>
2022-11-07 17:35:56 -0300

    Adiciona s-data-table

---
+++ src/components/SDataTable/SDataTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="overflow-x-auto relative">
+    <table class="table">
+      <thead>
+        <slot name="t-head" />
+      </thead>
+      <tbody>
+        <slot name="t-body" />
+      </tbody>
+    </table>
+  </div>
+</template>
+<script lang="ts" setup>
+
+const props = defineProps({
+
+})
+
+</script>
+<style lang="postcss">
+.table {
+  @apply w-full text-sm text-left text-stone-500 dark:text-stone-400;
+}
+
+.table th,
+.table td {
+  @apply py-3 px-6;
+}
+
+.table thead {
+  @apply text-xs border-b border-stone-500/50 text-stone-700 dark:text-stone-400;
+}
+
+.table tbody tr {
+  @apply hover:bg-stone-600
+}
+
+.table tr:not(:last-child) {
+  @apply border-b border-stone-500/50;
+}
+</style>
