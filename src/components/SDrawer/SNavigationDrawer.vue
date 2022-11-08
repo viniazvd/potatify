31eab41cc922a8f090fb88514d8baabfe21771e2
Allan Lima <allan.lima@seedz.ag>
2022-11-08 17:44:34 -0300

    Ajusta o funcionamento dos componentes

--- src/components/SDrawer/SNavigationDrawer.vue
+++ src/components/SDrawer/SNavigationDrawer.vue
@@ -1,45 +1,23 @@
 <template>
-  <Teleport to="#app">
-    <STransition name="SLIDE_IN_LEFT">
-      <div
-        ref="drawer-navigation"
-        class="s-navigation-drawer border-r border-stone-500/20"
-        :style="[width]"
-        tabindex="-1"
-        v-show="props.isOpen"
-        aria-labelledby="drawer-navigation-label"
-      >
-        <slot />
-      </div>
-    </STransition>
-  </Teleport>
+  <STransition name="SLIDE_IN_LEFT">
+    <SSidebar v-show="isOpen" :size="props.size">
+      <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
+    </SSidebar>
+  </STransition>
 </template>
 <script lang="ts" setup>
-import {computed} from "vue";
 import STransition from "@components/STransition/STransition.vue";
 
+import SSidebar from "@components/SSidebar/SSidebar.vue"
+
 const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false
+  },
   size: {
     type: Number,
     default: 320
-  },
-  isOpen: {
-    type: Boolean,
-    default: true
-  },
-  permanent: {
-    type: Boolean,
-    default: true
   }
 })
-
-const width = computed(() => ({
-  width: `${props.size}px`
-}))
-
 </script>
-<style lang="postcss" scoped>
-.s-navigation-drawer {
-  @apply fixed top-0 left-0 z-50 h-screen overflow-y-auto bg-white dark:bg-stone-800;
-}
-</style>
