97d323262ac8bbb4e00db6549cc114b1c7c670c3
Allan Lima <allan.lima@seedz.ag>
2022-11-08 16:28:27 -0300

    Adiciona o componente s-chip

---
+++ src/components/SChip/SChip.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="flex flex-wrap justify-center space-x-2 chips">
+    <span
+      class="rounded-full text-gray-500 bg-gray-200 font-semibold text-sm flex align-center w-max cursor-pointer active:bg-gray-300 transition duration-300 ease select-none flex items-center gap-1"
+      :class="[clearable, withSize]"
+      >
+      <slot name="image">
+        <img src="" alt="" :class="[withImage]" />
+      </slot>
+      <slot />
+
+      <slot name="clearable">
+        <button @click="emit('click:clear')" v-if="props.clearable" class="bg-transparent hover:bg-gray-400 rounded-full hover focus:outline-none transition">
+          <SIcon icon="mdi:close" class="text-gray-500" />
+        </button>
+      </slot>
+    </span>
+</div>
+</template>
+<script lang="ts" setup>
+import { computed } from '@vue/reactivity';
+import { defineAsyncComponent, PropType } from 'vue';
+
+const SIcon = defineAsyncComponent(() => import("@components/SIcon/Index.vue"));
+
+const props = defineProps({
+  clearable: Boolean,
+  size: {
+    type: String as PropType<"sm" | "md" | "lg">,
+    default: "md"
+  },
+  imgSrc: String
+})
+
+const emit = defineEmits(["click:clear"])
+
+const clearable = computed(() => props.clearable && "clearable");
+
+const sizeOptions = {
+  sm: "small",
+  md: "medium",
+  lg: "large"
+} as const;
+
+const withSize = computed(() => sizeOptions[props.size])
+
+const withImage = computed(() => props.imgSrc && "chip-image")
+
+</script>
+<style lang="postcss" scoped>
+.chips .clearable  {
+  @apply pr-2 !important;
+}
+
+.chips .small {
+  @apply px-2 py-1;
+}
+
+.chips .small.clearable {
+  @apply pr-1 !important
+}
+
+.chips .medium {
+  @apply px-4 py-2;
+}
+
+
+.chips .large {
+  @apply px-5 py-3;
+}
+
+.chips .large.clearable {
+  @apply pr-3 !important
+}
+
+</style>
