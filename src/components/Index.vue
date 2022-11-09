d6429f6a6b94c8bf6bf2f5b89352c68ff047d934
viniazvd <viniazvd@gmail.com>
2022-11-09 15:38:19 -0300

    :zap: add radio-group example

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,38 +1,24 @@
 <template>
   <div class="m-12">
-    <h1>{{ count }}</h1>
-
-    <div class="flex w-full gap-4">
-      <div class="flex-[100vw/12]"></div>
-      <div class="bg-primary-200">Conteudo</div>
-    </div>
-
-    <div>
-      <SSelectV2
-        label="Bater almoço"
-        item-value="id"
-        item-text="name"
-        :items="items"
-        return-object
-        v-model="selectedItem"
-      />
-    </div>
+    <s-radio-group
+      row
+      label="label test"
+      :options="options"
+      v-model="activeRadio"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import SInput from "./SInput/SInputV2.vue"
-import SSelectV2 from "@components/SSelect/SSelectV2.vue";
+import SRadioGroup from "@components/SRadioGroup/Index.vue";
 
-const selectedItem = ref();
+const activeRadio = ref('111')
 
-const items = ref([
-  { name: "Allan", id: 1 },
-  { name: "Vinicius", id: 2 },
-  { name: "Laise", id: 3 },
-  { name: "Perera", id: 4 },
+const options = ref([
+  { id: '1', label: '111', value: '111' },
+  { id: '2', label: '222', value: '222', disabled: true },
+  { id: '3', label: '333', value: '333' },
+  { id: '4', label: '444', value: '444' },
 ])
-
-const count = ref(0)
 </script>
