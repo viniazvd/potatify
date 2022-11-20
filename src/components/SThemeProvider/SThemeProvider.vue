78ae4ee05b26c775038c8eb3d29d7eab31851664
Allan Lima <allan.lima@seedz.ag>
2022-11-20 02:04:02 -0300

    Adiciona o theme provider

---
+++ src/components/SThemeProvider/SThemeProvider.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :id="`theme-${uniqueId}`" class="light">
+    <slot v-bind="{ isDark, toggleDarkTheme }"></slot>
+  </div>
+</template>
+<script lang="ts" setup>
+import {useDark, useToggle} from "@vueuse/core";
+import {useUUID} from "@/composables";
+
+const { uniqueId } = useUUID();
+
+const props = defineProps({
+  dark: Boolean
+})
+
+const isDark =  useDark({
+  selector: `#theme-${uniqueId}`,
+  initialValue: props.dark ? "dark" : "light"
+})
+
+const toggleDark = useToggle(isDark)
+
+function toggleDarkTheme () {
+  toggleDark()
+}
+
+</script>
