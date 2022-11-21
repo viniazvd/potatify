84a5ac3843181ad462985d9ddeec6d0682998d18
Allan Lima <allan.lima@seedz.ag>
2022-11-21 14:49:48 -0300

    Ajusta a configuração de alias no storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,6 @@
+const path =  require("path");
+const tsconfigPaths = require("vite-tsconfig-paths").default;
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -16,5 +19,16 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: async (config) => {
+    config.plugins.push(
+        /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+        tsconfigPaths({
+          /** @see https://github.com/storybookjs/builder-vite/issues/85#issuecomment-1119483129 */
+          projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+        })
+    );
+
+    return config;
   },
 }
