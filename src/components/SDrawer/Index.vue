ba8b0907e1979aa02cb26f2f36c0541ef77dbe5e
viniazvd <viniazvd@gmail.com>
2022-11-22 14:52:50 -0300

    :fire: remove sidebar

--- src/components/SDrawer/Index.vue
+++ src/components/SDrawer/Index.vue
@@ -1,23 +1,39 @@
 <template>
   <STransition name="SLIDE_IN_LEFT">
-    <SSidebar v-show="isOpen" :size="props.size">
-      <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
-    </SSidebar>
+    <div
+      v-show="props.isOpen"
+      :style="[width]"
+      tabindex="-1"
+      class="s-drawer border-r border-stone-500/20"
+      aria-labelledby="drawer-navigation-label"
+    >
+      <slot />
+    </div>
   </STransition>
 </template>
-<script lang="ts" setup>
-import STransition from "@components/STransition/Index.vue";
 
-import SSidebar from "@components/SSidebar/SSidebar.vue"
+<script lang="ts" setup>
+import STransition from '@components/STransition/Index.vue'
+import { computed } from 'vue';
 
 const props = defineProps({
-  isOpen: {
-    type: Boolean,
-    default: false
-  },
+  isOpen: Boolean,
+
+  // noOverlay: Boolean,
+
   size: {
     type: Number,
     default: 320
   }
 })
+
+const width = computed(() => ({
+  width: `${props.size}px`
+}))
 </script>
+
+<style lang="postcss" scoped>
+.s-drawer {
+  @apply fixed top-0 left-0 z-50 h-screen overflow-y-auto bg-white dark:bg-stone-800;
+}
+</style>
