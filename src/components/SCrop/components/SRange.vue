5f944669897cd1d0f65780352a26da84b92600d0
viniazvd <viniazvd@gmail.com>
2022-11-28 21:40:46 -0300

    init

---
+++ src/components/SCrop/components/SRange.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="range">
+    <button class="decrease" @click="onDecrease" />
+
+    <input
+      step="1"
+      type="range"
+      class="input"
+      :value="value"
+      :min="min"
+      :max="max"
+      @input="onChange"
+    >
+
+    <button class="increase" @click="onIncrease">
+      <span></span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// @ts-nocheck
+const props = defineProps({
+  value: Number,
+  min: Number,
+  max: Number
+})
+
+const emit = defineEmits(['input'])
+
+function onChange (event) {
+  emit('input', +event.target.value)
+}
+
+function onDecrease () {
+  const value = props.value - props.max / 4
+
+  emit('input', value <= props.min ? props.min : value)
+}
+
+function onIncrease () {
+  const value = props.value + props.max / 4
+
+  emit('input', value >= props.max ? props.max : value)
+}
+</script>
+
+<style lang="scss">
+.range {
+  margin-top: 40px;
+  display: flex;
+  align-items: center;
+
+  & > .input {
+    height: 8px;
+    width: 180px;
+    background: transparent;
+    -webkit-appearance: none;
+
+    &:focus { outline: none; }
+
+    // bar
+    &::-webkit-slider-runnable-track {
+      height: 8px;
+      width: 180px;
+      cursor: pointer;
+      border-radius: 25px;
+      background-color: #d2d4db;
+    }
+
+    // ball
+    &::-webkit-slider-thumb {
+      width: 14px;
+      height: 14px;
+      cursor: pointer;
+      margin-top: -4px;
+      border-radius: 15px;
+      background: #FFFFFF;
+      border: 0;
+      box-shadow: 0 0 11px 3px rgba(#121E48, 0.1);
+      -webkit-appearance: none;
+    }
+  }
+
+  & > .increase,
+  & > .decrease {
+    width: 25px;
+    height: 25px;
+    border-radius: 25px;
+    display: inline-block;
+    margin: 0 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    border: 0;
+    outline: none;
+    position: relative;
+    padding: 0;
+    background: linear-gradient(135deg, #BC4CF7 0%, #7873EE 100%);
+    & > span {
+      background: linear-gradient(135deg, #BC4CF7 0%, #7873EE 100%);
+      content: "";
+      display: block;
+      position: absolute;
+      left: 50%;
+      top: calc(50% + 4px);
+      width: 93%;
+      height: 75%;
+      border-radius: inherit;
+      filter: blur(6px);
+      opacity: 0.6;
+      transform: translateX(-50%) translateY(-50%) translateZ(-1px);
+      transition: filter .3s, opacity .3s;
+      flex: 1;
+    }
+    &:hover {
+      & > span {
+        filter: blur(10px);
+        opacity: 1;
+      }
+    }
+  }
+
+  & > .decrease {
+    &:before {
+      content: "";
+      width: 10px;
+      height: 1px;
+      background: white;
+      display: inline-block;
+    }
+  }
+
+  & > .increase {
+    &:before,
+    &:after {
+      content: "";
+      width: 10px;
+      height: 1px;
+      background: white;
+      display: inline-block;
+    }
+    &:after{
+      position: absolute;
+      transform: rotate(90deg);
+    }
+  }
+}
+</style>
---
+++ src/components/SCrop/components/SSelector.vue
@@ -0,0 +1,58 @@
+<template>
+  <svg
+    fill="white"
+    class="w-full h-full absolute top-0 left-0 right-0 bottom-0 rounded-sm"
+  >
+    <defs>
+      <filter id="shadow">
+        <feDropShadow dx="0" dy="4" stdDeviation="4" flood-opacity=".5" />
+      </filter>
+
+      <clipPath id="mask">
+        <circle :r="props.selector / 2" :cx="props.coordinates.x" :cy="props.coordinates.y" />
+      </clipPath>
+    </defs>
+
+    <circle
+      class="selector"
+      stroke-width="2"
+      stroke="#FFFFFF"
+      filter="url(#shadow)"
+      :r="props.selector / 2"
+      :cx="props.coordinates.x"
+      :cy="props.coordinates.y"
+    />
+
+    <image
+      opacity=".5"
+      width="100%"
+      height="100%"
+      preserveAspectRatio="none"
+      :href="src"
+      :style="{ transform: `scale(${+scale.toFixed(1)})` }"
+    />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+// @ts-nocheck
+const props = defineProps({
+  src: String,
+  selector: Number,
+  coordinates: Object,
+  scale: { type: Number, default: 1 }
+})
+</script>
+
+<style lang="scss">
+.wrapper-selector {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: absolute;
+  border-radius: 5px;
+}
+</style>
