043cc50dc3fccbff7cbbd09204f87d450b547f38
Allan Lima <allan.lima@seedz.ag>
2023-01-09 11:14:28 -0300

    Adiciona os types para as props do sSinput

--- src/components/SInput/Index.vue
+++ src/components/SInput/Index.vue
@@ -1,21 +1,32 @@
 <template>
-  <SInputContainer
-    v-bind="props"
-  />
+  <SInputContainer v-bind="props"/>
 </template>
 
 <script lang="ts">
 // interface Props extends InputContainerType {}
 
 export default {
-  name: "SInput"
+  name: "SInput",
+  inheritAttrs: false
 }
 </script>
 
 <script lang="ts" setup>
 import SInputContainer from "../SInputContainer/Index.vue";
 import { defineProps } from "vue";
-// import {InputContainerType} from "@components/SInputContainer/SInputContainer.types";
 
-const props:any = defineProps();
+const props = defineProps<{
+  label: string,
+  modelValue?: any,
+  required?: boolean,
+  appendInnerIcon?: string,
+  appendOuterIcon?: string,
+  prependInnerIcon?: string,
+  prependOuterIcon?: string,
+  hint?: string,
+  errorMessage?: string,
+  persistentHint?: boolean,
+  rules?: Array<(value: string) => boolean | string>,
+  eager?: boolean
+}>()
 </script>
