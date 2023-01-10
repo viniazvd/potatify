e7c7bcbecf9d8059eafcc8169d8ebb41b84e17ca
Vinicius Azevedo <viniazvd@gmail.com>
2023-01-10 18:29:11 -0300

    :art: feedback example

--- src/App.vue
+++ src/App.vue
@@ -2,66 +2,29 @@
   <main class="flex flex-col gap-4 p-4">
     <router-view></router-view>
 
-    <button @click="isLoading = !isLoading">toggle</button>
-    <s-button :loading="isLoading" :disabled="isLoading">Loader button</s-button>
+    <transition-group tag="div" name="s-feedback" class="absolute right-3 top-3 flex flex-col-reverse gap-px">
+      <s-feedback
+        v-for="item in feedbacks"
+        :key="item.id"
+        :id="item.id"
+        :type="item.type"
+        :title="item.title"
+        :message="item.message"
+        :duration="item.duration"
+        :prepend-icon="item.prependIcon"
+        :auto-close="item.autoClose"
+        @close="removeFeedbacks(item.id)"
+      />
+    </transition-group>
   </main>
 </template>
 
 <script setup lang="ts">
-import SList from '@components/SList/Index.vue'
-import STabber from '@components/STabber/Index.vue'
-import SButton from '@components/SButton/Index.vue'
-import SIcon from '@components/SIcon/Index.vue'
-import SDropdown from '@components/SDropdown/Index.vue'
-import { Tab } from '@components/STabber/interfaces/index'
-import SHorizontalTimeline from "@components/STimeline/HorizontalTimeline.vue";
-import { ref } from 'vue';
-import SCardV2 from "@components/SCard/SCardV2.vue";
+import { provide } from 'vue';
+import SFeedback from "@components/SFeedback/Index.vue";
+import { useFeedback } from "@/composables/useFeedback";
 
-const isLoading = ref(false)
-const tabs = ref<Tab[]>([
-  { id: 1, name: 'tab 1', icon: 'mdi:account' },
-  { id: 2, name: 'tab 2', icon: 'uit:arrow-circle-left', disabled: true },
-  { id: 3, name: 'tab 3', icon: 'uit:arrow-circle-right' },
-  { id: 4, name: 'tab 4', icon: 'uit:arrow-circle-left' },
-  { id: 5, name: 'tab 5', icon: 'uit:arrow-circle-right' }
-])
-
-const item = ref({ icon: 'material-symbols:star-outline-rounded', text: 'Item sozinho boladao', disabled: true })
-
-const itemGroup = ref({
-  icon: 'material-symbols:star-outline-rounded',
-  text: 'Item grupo boladao',
-  items: [
-    { icon: 'material-symbols:star-outline-rounded', text: 'Item 11', disabled: true },
-    { icon: 'material-symbols:star-outline-rounded', text: 'Item 2' },
-  ]
-})
-
-const items = ref([
-  { icon: 'material-symbols:star-outline-rounded', text: 'Item 1' },
-  {
-    icon: 'material-symbols:star-outline-rounded',
-    text: 'Item 2',
-    active: true,
-    items: [
-      { icon: 'material-symbols:star-outline-rounded', text: 'Item 211', disabled: true },
-      { icon: 'material-symbols:star-outline-rounded', text: 'Item 22' },
-    ]
-  },
-  { icon: 'material-symbols:star-outline-rounded', text: 'Item 3', items: [
-      { icon: 'material-symbols:star-outline-rounded', text: 'Item 211', disabled: true },
-      { icon: 'material-symbols:star-outline-rounded', text: 'Item 22' },
-    ] },
-  { icon: 'material-symbols:star-outline-rounded', text: 'Item 4' },
-])
-const activeTab = ref(tabs.value[0])
+const { feedbacks, createFeedback, removeFeedbacks } = useFeedback()
+provide('create-feedback', createFeedback)
 
 </script>
-<style>
-html {
-  /*background: url("./assets/wallpaper-challenge.jpg");*/
-  /*background-size: cover;*/
-  /*background-position-x: -450px;*/
-}
-</style>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="m-12">
+    <s-button @click="createInfoFeedback">Info</s-button>
+    <s-button @click="createErrorFeedback">Error</s-button>
+    <s-button @click="createWarningFeedback">Warning</s-button>
+    <s-button @click="createSuccessFeedback">Success</s-button>
+    <s-button @click="createPersistentFeedback">Persistent</s-button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { inject } from "vue"
+import { CreateFeedback } from "@/composables/useFeedback";
+
+import SButton from "@components/SButton/Index.vue"
+
+const createFeedback = inject('create-feedback') as CreateFeedback
+
+const createInfoFeedback = () => createFeedback({ type: 'info', message: 'my message boladonaaaaaaaaaaa dasdasdasdsd dasdsdasdad' })
+const createWarningFeedback = () => createFeedback({ type: 'warning', prependIcon: 'material-symbols:close', message: 'Warning' })
+const createErrorFeedback = () => createFeedback({ type: 'error', message: 'Error', duration: 10 })
+const createSuccessFeedback = () => createFeedback({ type: 'success', message: 'Success', duration: 5 })
+const createPersistentFeedback = () => createFeedback({ type: 'info', message: 'persistent', autoClose: false })
+
 </script>
+
 <style scoped>
 
 /* ISSO PODE SER UM UTIL PRA BLUR BACKDROP */
